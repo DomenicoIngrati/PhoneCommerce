007d7d3df347c040276a3bedd8ca9ab47020e943
Roky97 <roccopalermiti@virgilio.it>
2019-03-13 18:37:31 +0100

    Added account registration

--- PhoneeCommerce/WebContent/js/account.js
+++ PhoneeCommerce/WebContent/js/account.js
@@ -77,13 +77,56 @@
 //  });
 //});
 
+(function($) {
+
+    $.fn.serializeFormJSON = function() {
+        var disabled = this.find(':input:disabled').removeAttr('disabled');
+        var o = {};
+        var a = this.serializeArray();
+        disabled.attr('disabled', 'disabled');
+        $.each(a, function() {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+})(jQuery);
+
+function operation_alert(result, callback) {
+    $("body").scrollTop(0);
+    $("#login-auto-close-alert").focus();
+    var str = result.result;
+    if ((str.search("SUCCESS")) != -1) {
+        $("#login-alert-text").html("<strong>" + result.message + "</strong>");
+        $("#login-auto-close-alert").removeClass("hidden").addClass("alert-success fade in");
+        $("#login-auto-close-alert").fadeTo(1500, 750).slideUp(600, function() {
+            $(this).removeClass("alert-success fade in");
+            callback();
+        });
+    } else {
+        $("#login-alert-text").html("<strong>" + result.reason + "</strong>");
+        $("#login-auto-close-alert").removeClass("hidden").addClass("alert-danger fade in");
+        $("#login-auto-close-alert").fadeTo(1500, 750).slideUp(600, function() {
+            $(this).removeClass("alert-danger fade in");
+            callback();
+        });
+    }
+    
+}
+
 $( document ).ready(function() {
 	
 	$("#sign-up").submit(function(e) {
 	    e.preventDefault();
-	    alert("ciao");
+	    
 	    var frm = $(this).serializeFormJSON();
-	    console.log(frm);
+	    
 	    $.ajax({
 	        url: "account?action=signup",
 	        type: "POST",
@@ -91,7 +134,7 @@ $( document ).ready(function() {
 	        data: JSON.stringify(frm)
 	    }).done(function(data) {
 	        operation_alert(data, function() {
-	            window.location.href = ""; //home
+	            window.location.href = "?action=index"; //home
 
 	        });
 
